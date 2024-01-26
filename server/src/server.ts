a734015150d15319e2f70d887c6ed13a7ef27a76
asr <asr12211@outlook.com>
2024-01-26 13:53:44 +0300

    ADD: socket functionality to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ import compression from "compression";
 import dotenv from "dotenv";
 import logger from "morgan";
 
+import { SocketUser } from "./types/types";
+import { WIN_ARRAYS } from "./constants/constants";
 
 const app = express();
 const server = createServer(app);
@@ -43,6 +45,96 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => res.statu
 // socket handling
 io.on("connection", (socket) => {
    
+   let player1: SocketUser | null = null;
+   let player2: SocketUser | null = null;
+   let turns = 0;
+
+   socket.on("join-room", (room) => {
+
+      let clientsInRoom = io.sockets.adapter.rooms.get(room)?.size || 0;
+
+      // if 2 players in room, don't accept any more
+      if (clientsInRoom === 2) return;
+
+      socket.join(room);
+      clientsInRoom = io.sockets.adapter.rooms.get(room)?.size || 0;
+
+      if (clientsInRoom === 2) {
+         player2 = {
+            userId: socket.id,
+         }
+         turns = 1;
+         socket.to(room).emit("game-start", turns);
+      } else if (clientsInRoom === 1) {
+         player1 = {
+            userId: socket.id,
+         };
+      }
+   });
+
+   socket.on("request-change", (room, playerObj) => {
+      let isError = false;
+      if (typeof room !== "string" || room.trim()) return;
+
+      if (!(playerObj || playerObj.userId || (playerObj.onBoardPieces && playerObj.onBoardPieces.length === 3) || playerObj.offBoardPieces)) return;
+
+      if (turns === 0) return;
+
+      if (turns === 1 && playerObj.userId !== player1?.userId) return;
+      if (turns === 2 && playerObj.userId !== player2?.userId) return;
+
+      playerObj.onBoardPieces.forEach((ele: number, i: number) => {
+         if (playerObj.userId === player1?.userId && player2 && player2.onBoardPieces && ele === player2.onBoardPieces[i]) {
+            socket.to(room).emit("not-allowed", playerObj.userId);
+            isError = true;
+         } else if (playerObj.userId === player2?.userId && player1 && player1.onBoardPieces && ele === player1.onBoardPieces[i]) {
+            socket.to(room).emit("not-allowed", playerObj.userId);
+            isError = true;
+         }
+      });
+
+      if (isError) return;
+
+      if (turns === 1) turns = 2;
+      if (turns === 2) turns = 1;
+
+      
+      WIN_ARRAYS.forEach(arr => {
+         let isWin = true;
+         arr.forEach((ele, i) => {
+            if (ele !== playerObj.onBoardPieces[i]) {
+               isWin === false;
+               socket.to(room).emit("state-change", playerObj, turns);
+               return;
+            }
+         });
+         if (isWin) {
+            socket.to(room).emit("state-change", playerObj, turns, playerObj.userId);
+            return;
+         }
+      });
+
+   });
+
+   socket.on("game-restart", (room) => {
+      
+      if (typeof room !== "string" || room.trim()) return;
+
+      turns = 1;
+      if (player1) {
+         player1.offBoardPieces = 3;
+         player1.onBoardPieces = [0, 0, 0];
+      }
+      if (player2) {
+         player2.offBoardPieces = 3;
+         player2.onBoardPieces = [0, 0, 0];
+      }
+
+      socket.to(room).emit("game-start", turns);
+   });
+
+   // the front shoud handle a player-out event
+
 });
 
 app.listen(process.env.PORT || 3000);
